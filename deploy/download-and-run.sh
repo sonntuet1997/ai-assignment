66cd234c8207b3dfab8a75f2cad9b82b768267e0
sonntuet1997@gmail.com <sonntuet1997@gmail.com>
2021-05-12 19:17:10 +0700

    update download-and-run.sh

--- deploy/download-and-run.sh
+++ deploy/download-and-run.sh
@@ -1,1 +1,4 @@
-docker-compose -f docker-compose-production.yml up -d --build --force-recreate
+docker-compose -f docker-compose-production.yml kill
+docker image rm $(docker-compose -f docker-compose-production.yml images -q) -f
+docker image prune -f
+docker-compose -f docker-compose-production.yml up -d --build --force-recreate -V
